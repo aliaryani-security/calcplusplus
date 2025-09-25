503f21ed07d40ebbb26b15d8547be4ce1ac85eb4
Ali Aryani <lenesis@proton.me>
2025-09-25 23:18:41 +0330

    operation functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
+void add (int num1, int num2) {
+   cout << num1 << " + " << num2 << " = " << (num1 + num2) << endl;
+}
+
+void subtract(int num1, int num2) {
+    cout << num1 << " - " << num2 << " = " << (num1 - num2) << endl;
+}
+
+void multiply(int num1, int num2) {
+    cout << num1 << " * " << num2 << " = " << (num1 * num2) << endl;
+}
+
+void divide(int num1, int num2){
+    cout << num1 << " / " << num2 << " = " << (num1 / num2) << endl;
+}
+
 void get_op () {
     int opt{};
     cout << "Choose the operation: " << endl;
