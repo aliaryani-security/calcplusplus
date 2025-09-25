ef2c8ea02e1608cf94afdc656f9f96f692031cfd
Ali Aryani <lenesis@proton.me>
2025-09-25 23:12:30 +0330

    basic get_opt structure

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 using namespace std;
 
+void get_op () {
+    int opt{};
+    cout << "Choose the operation: " << endl;
+    cout << "1. Addition" << endl 
+        << "2. Subtraction" << endl 
+        << "3. Multiplication" << endl
+        << "4. Division" << endl;
+    cin >> opt;
+    switch (opt) {
+        case 1:{} break;
+        case 2:{} break;
+        case 3:{} break;
+        case 4:{} break;
+        default:{
+                    cout << "not a valid option!" << endl;
+        } break;
+    }
+}
+
 int main() {
 
     return 0;
