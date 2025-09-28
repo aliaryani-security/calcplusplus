d5e6cfcb1cc2c6ff338cc94a73e5a6240581814d
Ali Aryani <lenesis@proton.me>
2025-09-28 22:29:17 +0330

    break original structure

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int get_num(bool first) {
     return num;
 }
 
-void get_op () {
+auto get_op () {
     int opt{};
     cout << "Choose the operation: " << endl;
     cout << "1. Addition" << endl 
@@ -22,30 +22,13 @@ void get_op () {
         << "3. Multiplication" << endl
         << "4. Division" << endl;
     cout << "=> ";
-    cin >> opt;
-    float num1 = get_num(true);
-    float num2 = get_num(false);
-    switch (opt) {
-        case 1:{
-                   cout << plus<>{}(num1,num2) << endl;
-               } break;
-        case 2:{
-                   cout << minus<>{}(num1,num2) << endl;
-               } break;
-        case 3:{
-                   cout << multiplies<>{}(num1,num2) << endl;
-               } break;
-        case 4:{
-                   cout << divides<>{}(num1,num2) << endl;
-               } break;
-        default:{
-                    cout << "not a valid option!" << endl;
-        } break;
-    }
 }
 
 int main() {
     get_op();
+    float num1 = get_num(true);
+    float num2 = get_num(false);
+
 
     return 0;
 }
